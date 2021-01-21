fc3c2e204fa3ab43dc661532ff2c034f7ff55d8f
uyjulian <uyjulian@gmail.com>
2021-01-21 11:06:56 -0600

    GCC 9.2.0, Binutils 2.25.1

--- scripts/001-binutils.sh
+++ scripts/001-binutils.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-# binutils-2.14.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
+# binutils.sh by Julian Uy (uyjulian@gmail.com)
+# Based on binutils-2.25.1.sh by SP193 (ysai187@yahoo.com)
+# Based on binutils-2.14.sh by Naomi Peori (naomi@peori.ca)
+# There is poor support for the "dvp" because I never worked with it
+# and don't actually understand why the old changes were necessary.
 
+BINUTILS_VERSION=2.25.1
 ## Download the source code.
-REPO_URL="https://github.com/ps2dev/binutils-gdb.git"
-REPO_FOLDER="binutils-gdb"
-BRANCH_NAME="iop-v2.14"
-if test ! -d "$REPO_FOLDER"; then
-	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
-else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+SOURCE=http://ftpmirror.gnu.org/binutils/binutils-$BINUTILS_VERSION.tar.bz2
+wget --continue $SOURCE || { exit 1; }
+
+## Unpack the source code.
+echo Decompressing Binutils $BINUTILS_VERSION. Please wait.
+rm -Rf binutils-$BINUTILS_VERSION && tar xfj binutils-$BINUTILS_VERSION.tar.bz2 || { exit 1; }
+
+## Enter the source directory and patch the source code.
+cd binutils-$BINUTILS_VERSION || { exit 1; }
+
+if [ -e ../../patches/binutils-$BINUTILS_VERSION-PS2.patch ]; then
+	cat ../../patches/binutils-$BINUTILS_VERSION-PS2.patch | patch -p1 || { exit 1; }
 fi
+
+target_names=("iop")
+targets=("mipsel-ps2-irx")
+extra_opts=("")
 
 OSVER=$(uname)
 if [ ${OSVER:0:10} == MINGW64_NT ]; then
@@ -30,19 +44,19 @@ fi
 echo "Building with $PROC_NR jobs"
 
 ## For each target...
-for TARGET in "iop"; do
+for ((i=0; i<${#target_names[@]}; i++)); do
+	TARG_NAME=${target_names[i]}
+	TARGET=${targets[i]}
+	TARG_XTRA_OPTS=${extra_opts[i]}
+
 	## Create and enter the build directory.
-	rm -rf build-$TARGET && mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
+	mkdir build-$TARG_NAME && cd build-$TARG_NAME || { exit 1; }
 
 	## Configure the build.
-	if [ ${OSVER:0:6} == Darwin ]; then
-		CC=/usr/bin/gcc CXX=/usr/bin/g++ LD=/usr/bin/ld CFLAGS="-O0 -ansi -Wno-implicit-int -Wno-return-type" ../configure --quiet --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
-	else
-		../configure --quiet --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
-	fi
+	../configure --quiet --disable-build-warnings --prefix="$PS2DEV/$TARG_NAME" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
 
 	## Compile and install.
-	make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -D_FORTIFY_SOURCE=0 -O2" LDFLAGS="$LDFLAGS -s" && make --quiet install && make --quiet clean || { exit 1; }
+	make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -O2" LDFLAGS="$LDFLAGS -s" && make --quiet install && make --quiet clean || { exit 1; }
 
 	## Exit the build directory.
 	cd .. || { exit 1; }
--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -1,54 +1,50 @@
 #!/bin/bash
-# gcc-3.2.3-stage1.sh by AKuHAK
-# Based on gcc-3.2.2-stage1.sh by Naomi Peori (naomi@peori.ca)
+# gcc-9.2.0-stage1.sh by uyjulian
+# Based on gcc-3.2.3-stage1.sh by AKuHAK
+# Based on gcc-3.2.2-stage1.sh by Dan Peori (danpeori@oopo.net)
 
-GCC_VERSION=3.2.3
+GCC_VERSION=9.2.0
 ## Download the source code.
-SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2
+SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
 wget --continue $SOURCE || { exit 1; }
 
 ## Unpack the source code.
 echo Decompressing GCC $GCC_VERSION. Please wait.
-rm -Rf gcc-$GCC_VERSION && tar xfj gcc-$GCC_VERSION.tar.bz2 || { exit 1; }
+rm -Rf gcc-$GCC_VERSION && tar xJf gcc-$GCC_VERSION.tar.xz || { exit 1; }
 
 ## Enter the source directory and patch the source code.
 cd gcc-$GCC_VERSION || { exit 1; }
+
 if [ -e ../../patches/gcc-$GCC_VERSION-PS2.patch ]; then
 	cat ../../patches/gcc-$GCC_VERSION-PS2.patch | patch -p1 || { exit 1; }
 fi
 
-OSVER=$(uname)
-## Apple needs to pretend to be linux
-if [ ${OSVER:0:6} == Darwin ]; then
-	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu"
-elif [ ${OSVER:0:10} == MINGW64_NT ]; then
-	TARG_XTRA_OPTS="--build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32"
-else
-	TARG_XTRA_OPTS=""
-fi
-
 ## Determine the maximum number of processes that Make can work with.
-if [ ${OSVER:0:5} == MINGW ]; then
-	PROC_NR=$NUMBER_OF_PROCESSORS
+## MinGW's Make doesn't work properly with multi-core processors.
+OSVER=$(uname)
+if [ ${OSVER:0:10} == MINGW32_NT ]; then
+	PROC_NR=2
 elif [ ${OSVER:0:6} == Darwin ]; then
 	PROC_NR=$(sysctl -n hw.ncpu)
 else
 	PROC_NR=$(nproc)
 fi
 
-echo "Building with $PROC_NR jobs"
+target_names=("iop")
+targets=("mipsel-ps2-irx")
+extra_opts=("--disable-libstdcxx")
 
 ## For each target...
-for TARGET in "iop"; do
+for ((i=0; i<${#target_names[@]}; i++)); do
+	TARG_NAME=${target_names[i]}
+	TARGET=${targets[i]}
+	TARG_XTRA_OPTS=${extra_opts[i]}
+
 	## Create and enter the build directory.
-	rm -rf build-$TARGET-stage1 && mkdir build-$TARGET-stage1 && cd build-$TARGET-stage1 || { exit 1; }
+	mkdir build-$TARG_NAME-stage1 && cd build-$TARG_NAME-stage1 || { exit 1; }
 
 	## Configure the build.
-	if [ ${OSVER:0:6} == Darwin ]; then
-		CC=/usr/bin/gcc CXX=/usr/bin/g++ LD=/usr/bin/ld CFLAGS="-O0 -ansi -Wno-implicit-int -Wno-return-type" ../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
-	else
-		../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
-	fi
+	../configure --quiet --prefix="$PS2DEV/$TARG_NAME" --target="$TARGET" --enable-languages="c" --disable-nls --disable-shared --disable-libssp --disable-libmudflap --disable-threads --disable-libgomp --disable-libquadmath --disable-target-libiberty --disable-target-zlib --without-ppl --without-cloog --with-headers=no --disable-libada --disable-libatomic --disable-multilib $TARG_XTRA_OPTS || { exit 1; }
 
 	## Compile and install.
 	make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -O2" LDFLAGS="$LDFLAGS -s" && make --quiet install && make --quiet clean || { exit 1; }
